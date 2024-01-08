afeb04d5ef7b50243a8292a9b003c0135c01ce1d
Denis Poczęty <49251639+Deniorrr@users.noreply.github.com>
2024-01-08 22:53:33 +0100

    disabling unused link

--- frontend/src/pages/Navbar.js
+++ frontend/src/pages/Navbar.js
@@ -61,6 +61,7 @@ export default function Navbar() {
               label="Test multiple functions"
               component={NavLink}
               to="/MetaHeuristicAlgorithmsTesterFrontend/testMultiple"
+              disabled
             />
             <Tab
               label="Add Algorithm"
