8d510129eaba12ddaf248b0d70d0fe4274ef8f7b
Denis Poczęty <49251639+Deniorrr@users.noreply.github.com>
2024-01-19 01:44:46 +0100

    litl more responsive

--- frontend/src/pages/TestMultipleAlgorithms.js
+++ frontend/src/pages/TestMultipleAlgorithms.js
@@ -334,7 +334,7 @@ function TestMultipleAlgorithms(props) {
   return (
     <Container style={{ marginTop: theme.spacing(2) }}>
       <Grid container spacing={2}>
-        <Grid item xs={4} className="aside">
+        <Grid item xs={12} sm={6} md={3} className="aside">
           <Grid container spacing={2}>
             <Grid item xs={12}>
               {renderAlgorithms()}
@@ -368,7 +368,7 @@ function TestMultipleAlgorithms(props) {
             ) : null}
           </Grid>
         </Grid>
-        <Grid item xs={8}>
+        <Grid item xs={12} sm={6} md={9}>
           <Card>
             <CardContent>
               <Typography variant="h5" component="div">
