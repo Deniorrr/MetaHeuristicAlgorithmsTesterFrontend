1fc1baa1938293f3c6bd256e81381f82f31a91f8
Denis Poczęty <dp303245@student.polsl.pl>
2024-01-21 01:17:32 +0100

    after laptop

--- frontend/src/pages/CheckSingle.js
+++ frontend/src/pages/CheckSingle.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "../components/apiConfig";
+import { Button } from "@mui/material";
 
 function CheckSingle() {
-  return <div>checkSingle</div>;
+  const [executedAlgorithms, setExecutedAlgorithms] = useState([]);
+
+  useEffect(() => {
+    fetchSingles();
+  }, []);
+
+  const sendContinueRequest = async () => {};
+
+  const renderAlgorithms = () => {
+    const elements = executedAlgorithms.map((executedAlgorithm) => {
+      return (
+        <>
+          <p>
+            {executedAlgorithm.testedAlgorithmName},
+            {executedAlgorithm.testedFitnessFunctionName},
+            {executedAlgorithm.fBest}
+            <Button
+              variant="outlined"
+              onClick={() => {
+                sendContinueRequest(executedAlgorithm.id);
+              }}
+            >
+              Continue
+            </Button>
+          </p>
+        </>
+      );
+    });
+    return elements;
+  };
+
+  const fetchSingles = async () => {
+    const response = await api.get("/executedSingleAlgorithms");
+    setExecutedAlgorithms(response.data.executedAlgorithms);
+    console.log(response.data.executedAlgorithms);
+  };
+  return (
+    <div>
+      <h1>Single Algorithms</h1>
+      {renderAlgorithms()}
+    </div>
+  );
 }
 
 export default CheckSingle;
